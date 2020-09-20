e2e75a0a92ac60e1a7216a9399a64f281c8c648c
Maxim Rudenko <maximu.rudenko@gmail.com>
2020-09-21 01:26:39 +0300

    services

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <Header :links="links" :phoneNumber="phoneNumber" />
-    <Promo/>
-    <About/>
+    <Promo />
+    <About />
+    <Services :serviceCards="serviceCards" />
   </div>
 </template>
 
@@ -10,9 +11,15 @@
 import Header from "@/components/Header.vue";
 import Promo from "@/components/Promo.vue";
 import About from "@/components/About.vue";
+import Services from "@/components/Services.vue";
 export default {
   name: "App",
-  components: { Header: Header, Promo: Promo, About: About },
+  components: {
+    Header: Header,
+    Promo: Promo,
+    About: About,
+    Services: Services
+  },
   data: () => {
     return {
       phoneNumber: {
@@ -34,6 +41,60 @@ export default {
         },
         {
           name: "Контакты"
+        }
+      ],
+      serviceCards: [
+        {
+          title: "Ремонт квартир под ключ",
+          text: "Товарищи! постоянное информационно-пропагандистское",
+          imageUrl: require("@/assets/services/7.jpg")
+        },
+        {
+          title: "Ремонт и отделка домов, коттеджей, таунхаусов",
+          text:
+            "Товарищи! постоянное информационно-пропагандистское обеспечение нашей деятельности в значительной степени",
+          imageUrl: require("@/assets/services/1.jpg")
+        },
+        {
+          title: "Перепланировка квартиры в новостроке",
+          text:
+            "Товарищи! постоянное информационно-пропагандистское обеспечение нашей деятельности в значительной степени обуславливает создание ",
+          imageUrl: require("@/assets/services/4.jpg")
+        },
+        {
+          title: "Ремонт в новостройке",
+          text:
+            "Товарищи! постоянное информационно-пропагандистское обеспечение нашей деятельности в значительной степени",
+          imageUrl: require("@/assets/services/3.jpg")
+        },
+        {
+          title: "Капитальный ремонт вторичного жилья",
+          text:
+            "Товарищи! постоянное информационно-пропагандистское обеспечение нашей деятельности в значительной степени",
+          imageUrl: require("@/assets/services/2.jpg")
+        },
+        {
+          title: "Ремонт ванной комнаты и с/у под ключ",
+          text:
+            "Товарищи! постоянное информационно-пропагандистское обеспечение нашей деятельности в значительной",
+          imageUrl: require("@/assets/services/9.jpg")
+        },
+        {
+          title: "Ремонт офисов и торговых помещений",
+          text:
+            "Товарищи! постоянное информационно-пропагандистское обеспечение нашей деятельности в значительной степени обуславливает создание ",
+          imageUrl: require("@/assets/services/5.jpg")
+        },
+        {
+          title: "Косметический ремонт",
+          text:
+            "Товарищи! постоянное информационно-пропагандистское обеспечение нашей деятельности в значительной степени обуславливает создание ",
+          imageUrl: require("@/assets/services/8.jpg")
+        },
+        {
+          title: "Отделка фасадов домов",
+          text: "Товарищи! постоянное информационно-пропагандистское",
+          imageUrl: require("@/assets/services/6.jpg")
         }
       ]
     };
---
+++ src/components/Services.vue
@@ -0,0 +1,97 @@
+<template>
+  <section class="services">
+    <h2 class="services__title">Услуги</h2>
+    <ul class="services__list">
+      <li
+        v-for="(serviceCard, index) in serviceCards"
+        :key="index"
+        class="services__card"
+      >
+        <img
+          :src="serviceCard.imageUrl"
+          :alt="serviceCard.title"
+          class="services__card-image"
+        />
+        <h3 class="services__card-title">{{ serviceCard.title }}</h3>
+        <p class="services__card-text">{{ serviceCard.text }}</p>
+      </li>
+    </ul>
+  </section>
+</template>
+<script>
+export default {
+  name: "Services",
+  props: ["serviceCards"],
+  data: () => {
+    return {};
+  }
+};
+</script>
+<style lang="stylus">
+.services
+    margin 0
+    padding 32px 0px 32px
+    overflow hidden
+    box-sizing border-box
+    background-color $lightGrey
+    text-align center
+    max-width 1280px
+    +tablet()
+        padding-top 64px
+        background-image url('~@/assets/services-background.svg')
+        background-repeat no-repeat
+        background-position right bottom
+    +desktop()
+        margin 0 auto
+    &__title
+        margin 0
+        padding 0
+        margin-left 16px
+        padding-left 6px
+        font-family robotoblack
+        font-size 28px
+        line-height 33px
+        color $darkGrey
+        border-left 9px solid $orange
+        text-align left
+        +tablet()
+            font-size 48px
+            line-height 62px
+    &__list
+        margin 0 auto
+        padding 0
+        width 100%
+        column 1
+        column-gap 10px
+        flex-direction column
+        align-items center
+        list-style none
+        +tablet()
+            column-count 2
+            column-gap 24px
+            width 727px
+        +desktop()
+            column-count 3
+            width 1103px
+    &__card
+        margin 0
+        padding 0
+        display inline-block
+        width 300px
+        background-color white
+        margin-top 24px
+        text-align left
+        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        +tablet()
+            width 351px
+    &__card-image
+        width 100%
+        height 74.93%
+        object-fit cover
+    &__card-title
+        margin 0
+        padding 16px
+    &__card-text
+        margin 0
+        padding 0 16px 16px
+</style>
